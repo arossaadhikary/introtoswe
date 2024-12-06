172d3d5b900c604c5c3a99f5fbf7bfd81d43f82b
houricky <richardhouston18@gmail.com>
2024-12-05 22:50:30 -0500

    Update navbar.jsx
    changes to UI

--- introtoswe/frontend/src/components/navbar.jsx
+++ introtoswe/frontend/src/components/navbar.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { useAuthStore } from "../store/useAuthStore";
-import { LogOut, MessageSquare, Settings, User } from "lucide-react";
+import { LogOut, MessageSquare, Settings, User, House } from "lucide-react";
 
 const Navbar = () => {
   const { logout, authUser } = useAuthStore();
@@ -13,18 +13,19 @@ const Navbar = () => {
       <div className="container mx-auto px-4 h-16">
         <div className="flex items-center justify-between h-full">
           <div className="flex items-center gap-8">
+            <Link to="/home" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
+              <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
+                <House className="w-5 h-5 text-primary" />
+              </div>
+              <h1 className="text-lg font-bold">Home</h1>
+            </Link>
             <Link to="/" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
               <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
                 <MessageSquare className="w-5 h-5 text-primary" />
               </div>
               <h1 className="text-lg font-bold">Chat</h1>
             </Link>
-            <Link to="/home" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
-              <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
-                <MessageSquare className="w-5 h-5 text-primary" />
-              </div>
-              <h1 className="text-lg font-bold">Home</h1>
-            </Link>
+            
           </div>
 
           <div className="flex items-center gap-8">
