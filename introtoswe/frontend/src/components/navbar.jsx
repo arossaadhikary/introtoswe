8684bc32132f6e7b2b757f15c261518276fdeaa2
houricky <richardhouston18@gmail.com>
2024-12-06 15:43:14 -0500

    UI changes

--- introtoswe/frontend/src/components/navbar.jsx
+++ introtoswe/frontend/src/components/navbar.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { useAuthStore } from "../store/useAuthStore";
-import { LogOut, MessageSquare, Settings, User, House, CirclePlus } from "lucide-react";
+import { LogOut, MessageSquare, Settings, User, ClipboardList, CirclePlus } from "lucide-react";
 
 const Navbar = () => {
   const { logout, authUser } = useAuthStore();
@@ -16,9 +16,9 @@ const Navbar = () => {
             {authUser && ( // Only show "Home" if the user is logged in
               <Link to="/home" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
                 <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
-                  <House className="w-5 h-5 text-primary" />
+                  <ClipboardList className="w-5 h-5 text-primary" />
                 </div>
-                <h1 className="text-lg font-bold">Home</h1>
+                <h1 className="text-lg font-bold">Tasks</h1>
               </Link>
             )}
             <Link to="/" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
